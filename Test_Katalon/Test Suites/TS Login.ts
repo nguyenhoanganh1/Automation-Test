61be85cd030236465c9839bf8451ff51310b3e95
nguyenhoanganh1 <hoanganh25022000@gmail.com>
2021-11-01 13:43:08 +0700

    ok

---
+++ Test_Katalon/Test Suites/TS Login.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>88b81578-47db-4bd2-8440-d1393feff477</testSuiteGuid>
+   <testCaseLink>
+      <guid>c96f6750-a6d0-49fb-80c6-a5ee4cd5d320</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f8464c5-efea-4979-b2a8-7ee4e7afb905</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC 2 Failure</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>be2574e0-2851-40c4-93ba-d981183fdfd9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC 3 Failure</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>41a0e9fc-64f8-4152-bd84-d6246fe28afe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC 4 Failure</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test_Katalon/Test Suites/TS ShoppingCart.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS ShoppingCart</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>46acc4ad-93a0-4f6d-9a37-3143a890563e</testSuiteGuid>
+   <testCaseLink>
+      <guid>226ddee1-462a-41b2-92d0-17dd4039c157</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShoppingCart/TC 1 Failure</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>132c3f38-de80-45a7-a77d-6e40e2a1b9f6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShoppingCart/TC 2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>510c3da4-0261-4ad6-81d3-6ad149cb727c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShoppingCart/TC 3 Failure</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bef6e577-ac75-4be7-bd10-bbfdfc882b6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShoppingCart/TC 4 Failure</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1934ecdf-fe5f-4b78-808a-9e3f99b5f2de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShoppingCart/TC 5</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cd6f30b9-dc05-4b60-bed3-60b5d00ddee1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShoppingCart/TC 6</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>afae2216-c162-4edd-8029-45c648c77e91</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShoppingCart/TC 7</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
